973305d285b80e16040d166f37e555b9b3cb9e65
Andreas Skovdal <andreasskovdal@gmail.com>
2023-05-08 12:34:05 +0200

    Add api dir with Flask server

---
+++ api/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, send_from_directory
+
+# The relative path for where the React app is located
+build_dir = "../web/build"
+
+# Initialize the Flask app, and tell it that the folder with static files is located in
+# the React app in "build_dir"
+app = Flask(__name__, static_url_path="", static_folder=build_dir)
+
+
+# The front page serves the React app that exists in "build_dir"
+@app.route("/")
+def serve_cra():
+    return send_from_directory(build_dir, "index.html")
+
+
+# The URL "/api/route" calculates the route and returns it. This URL is called by the
+# React app, which displays it on the frontend.
+@app.route("/api/route")
+def create_route():
+    return "<p>Route goes here</p>"
